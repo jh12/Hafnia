3ded8f0c0ff675b448403eab015467d949403749
jh12 <jabaha35@hotmail.com>
2022-04-22 16:48:02 +0200

    Add project and submodule

---
+++ src/Hafnia/Program.cs
@@ -0,0 +1,26 @@
+﻿using Akka.Actor;
+using Serilog;
+using Serilog.Core;
+
+CancellationTokenSource cts = new CancellationTokenSource();
+
+Logger logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .MinimumLevel.Information()
+    .CreateLogger();
+
+const string hoconConfig = "akka { loglevel=DEBUG, loggers=[\"Akka.Logger.Serilog.SerilogLogger, Akka.Logger.Serilog\"] }";
+
+using (ActorSystem system = ActorSystem.Create("hafnia-system", hoconConfig))
+{
+    logger.Information("Press CTRL+C or send sigterm to exit");
+
+    Console.CancelKeyPress += (sender, args) =>
+    {
+        logger.Information("Termination requested");
+        cts.Cancel();
+        args.Cancel = true;
+    };
+
+    cts.Token.WaitHandle.WaitOne();
+}
