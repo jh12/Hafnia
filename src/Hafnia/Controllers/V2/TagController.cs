3022ccb5367951b11da9cde30f0fbd5373908d37
jh12 <jabaha35@hotmail.com>
2023-11-25 15:45:26 +0100

    Add endpoint for getting tags by name

--- src/Hafnia/Controllers/V2/TagController.cs
+++ src/Hafnia/Controllers/V2/TagController.cs
@@ -20,4 +20,13 @@ public class TagController : ControllerBase
     {
         return _tagRepository.GetTagsAsync(cancellationToken);
     }
+
+    [HttpGet("fromNames")]
+    public async Task<ActionResult<Tag[]>> GetFromNames(string names, CancellationToken cancellationToken)
+    {
+        HashSet<string> nameSet = names.Split(",", StringSplitOptions.TrimEntries).ToHashSet();
+
+        return await _tagRepository.GetTagsAsync(cancellationToken)
+            .Where(t => nameSet.Contains(t.Name)).ToArrayAsync(cancellationToken: cancellationToken);
+    }
 }
