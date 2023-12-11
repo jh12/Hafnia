ee709de5b4d734e1adf7eb137f96af1d00fd5b37
jh12 <jabaha35@hotmail.com>
2023-12-11 16:20:36 +0100

    Work on frontend

---
+++ src/Hafnia.Frontend/Components/CollectionCard.razor.cs
@@ -0,0 +1,42 @@
+﻿using Hafnia.DTOs;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace Hafnia.Frontend.Components;
+
+public partial class CollectionCard
+{
+    private string? _styleString;
+    private string? _width;
+    private string? _height;
+
+    [Parameter]
+    public string? Width { set; get; }
+
+    [Parameter]
+    public string? Height { set; get; }
+
+    [Parameter]
+    public string? Style { set; get; }
+
+    [Parameter]
+    public Collection Collection { get; set; } = null!;
+
+    [Parameter]
+    public EventCallback<MouseEventArgs> OnClick { get; set; }
+
+    protected override void OnInitialized()
+    {
+        if (!string.IsNullOrEmpty(Width))
+        {
+            _width = $"width:{Width};";
+        }
+
+        if (!string.IsNullOrEmpty(Height))
+        {
+            _height = $"height:{Height};";
+        }
+
+        _styleString = $"{_width}{_height}{Style}";
+    }
+}
--- src/Hafnia.Frontend/Repositories/TagRepository.cs
+++ src/Hafnia.Frontend/Repositories/TagRepository.cs
@@ -7,13 +7,36 @@ internal class TagRepository
 {
     private readonly HttpClient _httpClient;
 
+    private TimeSpan _refreshInterval = TimeSpan.FromMinutes(5);
+    private DateTime? _lastRefreshTime;
+    private Tag[] _tags = Array.Empty<Tag>();
+
+    private SemaphoreSlim _semaphore = new(1);
+
     public TagRepository(HttpClient httpClient)
     {
         _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
     }
 
-    public Task<Tag[]> GetAllTagsAsync()
+    public async Task<Tag[]> GetAllTagsAsync()
     {
-        return _httpClient.GetFromJsonAsync<Tag[]>("v2/tag")!;
+        await _semaphore.WaitAsync();
+
+        try
+        {
+            if (_lastRefreshTime.HasValue && DateTime.UtcNow < _lastRefreshTime.Value.Add(_refreshInterval))
+                return _tags;
+
+            _tags = (await _httpClient.GetFromJsonAsync<Tag[]>("v2/tag"))!;
+            _lastRefreshTime = DateTime.UtcNow;
+
+            return _tags;
+        }
+        finally
+        {
+            _semaphore.Release();
+        }
+
+        
     }
 }
